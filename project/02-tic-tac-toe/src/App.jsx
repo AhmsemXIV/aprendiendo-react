cafd02a29cb9b63871bb2f76ff12601fc13ccdeb
Juan José Benito Mateos <jjosebenitom@gmail.com>
2023-09-02 18:39:54 +0200

    Formateo

--- project/02-tic-tac-toe/src/App.jsx
+++ project/02-tic-tac-toe/src/App.jsx
@@ -1,101 +1,82 @@
-import { useState } from 'react'
-import './App.css'
-import { TURNS } from './constants'
-import confetti from 'canvas-confetti'
-import { Square } from './components/Square'
-import {checkEndGame,checkWinnerFrom} from './logic/board'
-import { WinnerModal } from './components/WinnerModal'
-
-
+import { useState } from 'react';
+import './App.css';
+import { TURNS } from './constants';
+import confetti from 'canvas-confetti';
+import { Square } from './components/Square';
+import { checkEndGame, checkWinnerFrom } from './logic/board';
+import { WinnerModal } from './components/WinnerModal';
 
 function App() {
-  
-  const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board')
-    return boardFromStorage ? JSON.parse(boardFromStorage) :
-    Array(9).fill(null)
-  })
+	const [board, setBoard] = useState(() => {
+		const boardFromStorage = window.localStorage.getItem('board');
+		return boardFromStorage
+			? JSON.parse(boardFromStorage)
+			: Array(9).fill(null);
+	});
 
-  const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-    return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X
-  })
+	const [turn, setTurn] = useState(() => {
+		const turnFromStorage = window.localStorage.getItem('turn');
+		return turnFromStorage ? JSON.parse(turnFromStorage) : TURNS.X;
+	});
 
-  const [winner, setWinner] = useState(null)
+	const [winner, setWinner] = useState(null);
 
+	const resetGame = () => {
+		setBoard(Array(9).fill(null));
+		setTurn(TURNS.X);
+		setWinner(null);
 
+		window.localStorage.removeItem('board');
+		window.localStorage.removeItem('turn');
+	};
 
-  const resetGame = () =>{
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
+	const updateBoard = (index) => {
+		if (board[index] || winner) return;
 
-    
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
-  }
+		const newBoard = [...board];
+		newBoard[index] = turn;
+		setBoard(newBoard);
 
-  const updateBoard = (index) => {
+		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+		setTurn(newTurn);
 
-    if(board[index] || winner) return
+		window.localStorage.setItem('board', JSON.stringify(newBoard));
+		window.localStorage.setItem('turn', JSON.stringify(newTurn));
 
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
-    
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
+		const newWinner = checkWinnerFrom(newBoard);
+		if (newWinner) {
+			confetti();
+			setWinner(newWinner);
+		} else if (checkEndGame(newBoard)) {
+			setWinner(false);
+		}
+	};
 
-    window.localStorage.setItem('board',JSON.stringify(newBoard))
-    window.localStorage.setItem('turn', JSON.stringify(newTurn))
+	return (
+		<main className='board'>
+			<h1>Tic Tac Toe</h1>
+			<button onClick={resetGame}>Resetear el juego</button>
+			<section className='game'>
+				{board.map((_, index) => {
+					return (
+						<Square
+							key={index}
+							index={index}
+							updateBoard={updateBoard}>
+							{board[index]}
+						</Square>
+					);
+				})}
+			</section>
 
-    const newWinner = checkWinnerFrom(newBoard)
-    if(newWinner){
-      confetti()
-      setWinner(newWinner)
-    } else if(checkEndGame(newBoard)){
-      setWinner(false)
-    }
-  }
+			<section className='turn'>
+				<Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+				<Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+			</section>
 
-  return (
-    <main className='board'>
-      <h1>Tic Tac Toe</h1>
-      <button onClick={resetGame}>Resetear el juego</button>
-      <section className='game'>
-        {
-          board.map((_, index) => {
-            return (
-              <Square 
-              key={index} 
-              index={index}
-              updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
-            )
-          })
-        }
-      </section>
-
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-          </Square>
-      </section>
-
-      <section>
-        {
-          WinnerModal(winner,resetGame)
-        }
-
-      </section>
-    </main>
-  )
-  
+			<section>{WinnerModal(winner, resetGame)}</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
--- project/02-tic-tac-toe/src/components/WinnerModal.jsx
+++ project/02-tic-tac-toe/src/components/WinnerModal.jsx
@@ -1,25 +1,21 @@
-import { Square } from "./Square"
+import { Square } from './Square';
 
 export function WinnerModal(winner, resetGame) {
+	if (winner === null) return null;
 
-    if(winner === null) return null
+	const winnerText = winner === false ? 'Empate' : 'Ganó' + winner;
 
-    const winnerText = winner === false ? 'Empate': 'Ganó' + winner
-
-    return (winner != null && (
-        <section className='winner'>
-          <div className='text'>
-            <h2>
-              {winnerText}
-            </h2>
-            <header className='win'>
-              {winner && <Square>{winner}</Square>}
-            </header>
-            <footer>
-              <button onClick={resetGame}>Empezar de nuevo</button>
-            </footer>
-          </div>
-        </section>
-      )
-      )
+	return (
+		winner != null && (
+			<section className='winner'>
+				<div className='text'>
+					<h2>{winnerText}</h2>
+					<header className='win'>{winner && <Square>{winner}</Square>}</header>
+					<footer>
+						<button onClick={resetGame}>Empezar de nuevo</button>
+					</footer>
+				</div>
+			</section>
+		)
+	);
 }
